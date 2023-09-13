2b8c6bc4e7ed3c1565387fc561fd66712036b41a
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-09-13 21:10:32 +0800

    the any type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ let sales: number = 123_456_789; // number
 let course: string = 'TypeScript'; // string
 let is_published: boolean = true; // true
 let level; // any
+
+function render(document: any) {
+    console.log(document)
+}
