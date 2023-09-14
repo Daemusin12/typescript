6c2ce25355f62d5ba712d7682fe3686c9273dd69
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-09-14 20:54:02 +0800

    functions

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,11 @@ let user: [number, string, Boolean, number] = [1, 'Leonel', true, 0]
 const enum Size { Small = 1, Medium, Large }; // pag const mas optimize yung js code
 let mySize: Size = Size.Medium;
 console.log(mySize);
+
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax(10_000)
