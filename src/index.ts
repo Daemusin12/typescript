cc8bafde91faa5a62c2ce8d72a8890740a12132a
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-09-15 13:40:45 +0800

    optional chaining

--- src/index.ts
+++ src/index.ts
@@ -80,3 +80,21 @@ function greet(name: string | null | undefined) {
 }   
 
 greet(undefined);
+
+type Customer = {
+    birthday: Date
+};
+
+function getCustomer(id: number): Customer | null {
+    return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(1);
+//Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+//Optional element access operator
+//customers?.[0]
+
+let log: any = null;
+log?.('a');
